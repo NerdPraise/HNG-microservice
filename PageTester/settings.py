c1c4cd81e1828c52fd16cd0c28833d4ca5d051af
Michael Ajanaku <remiljw@gmail.com>
2020-06-13 09:58:39 +0100

    collected dtatic

--- PageTester/settings.py
+++ PageTester/settings.py
@@ -152,4 +152,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
